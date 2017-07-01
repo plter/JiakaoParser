e3e2eef0340809e5e64308ef870cc2001f86759c
梦宇 <xtiqin@163.com>
2017-07-01 13:20:33 +0800

    Create parser.js

---
+++ parser.js
@@ -0,0 +1,104 @@
+/**
+ * Created by plter on 6/30/17.
+ */
+
+(function () {
+
+    var questions = [];
+    var questionsJsonOutput;
+    var currentTimerId = -1;
+
+    function buildUI() {
+        $('body').append(
+            "<div style='position: absolute;top: 500px;z-index: 10000'>" +
+            "   <div>" +
+            "       <button class='btn-start-get-info'>Start to get question info</button>" +
+            "       <button class='btn-print-data' style='margin-left: 20px;'>Print data</button>" +
+            "   </div>" +
+            "   <textarea class='questions-json-output' style='z-index: 10000;'></textarea>" +
+            "</div>");
+
+        questionsJsonOutput = $(".questions-json-output");
+        questionsJsonOutput.hide();
+    }
+
+    function retrieveQuestionInfo() {
+        var titleSource = $(".shiti-content").html();
+        var title = titleSource.substr(titleSource.indexOf(".") + 1).trim();
+
+        var optionsContainer = $(".options-container");
+        var options = optionsContainer.find("span");
+        var type = "Unknown";
+        var a, b, c, d;
+        a = options[0].innerHTML;
+        b = options[1].innerHTML;
+        switch (options.length) {
+            case 4:
+                type = "single";
+                c = options[2].innerHTML;
+                d = options[3].innerHTML;
+                break;
+            case 2:
+                type = 'judge';
+                break;
+        }
+        var answer = optionsContainer.find(".dui").find("span").html();
+        answer = answer.substring(0, answer.indexOf(".")).trim();
+
+        var description = $(".explain-container *[data-item='explain-content']").html();
+        if (description) {
+            description = description.trim();
+        }
+        var image = $(".media-container img").attr("src");
+
+        var question = {
+            type: type,
+            title: title,
+            a: a,
+            b: b,
+            c: c,
+            d: d,
+            answer: answer,
+            image: image,
+            description: description
+        };
+
+        questions.push(question);
+        console.log("Retrieve question " + question.title);
+
+        var tag = titleSource.substring(0, titleSource.indexOf("."));
+        var tokens = tag.split("/");
+        if (tokens[0].trim() === tokens[1].trim()) {
+            if (currentTimerId !== -1) {
+                clearInterval(currentTimerId);
+                alert("完成");
+            }
+        }
+    }
+
+    function addListeners() {
+        var nextBtn = $(".shiti-buttons button[data-item='next']");
+
+        $(".btn-start-get-info").click(function () {
+            retrieveQuestionInfo();
+
+            if (currentTimerId === -1) {
+                currentTimerId = setInterval(function () {
+                    nextBtn.click();
+                    setTimeout(retrieveQuestionInfo, 100);
+                }, 300);
+            }
+        });
+        $(".btn-print-data").click(function () {
+            questionsJsonOutput.show();
+            questionsJsonOutput.val(JSON.stringify({questions: questions}));
+        });
+    }
+
+    function init() {
+        buildUI();
+        addListeners();
+    }
+
+    init();
+})();
